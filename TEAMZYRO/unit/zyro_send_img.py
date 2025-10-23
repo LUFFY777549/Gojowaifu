c5a2652174e86e46202e73bd25afa5fd9c519266
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-23 20:34:43 +0530

    Update zyro_send_img.py

--- TEAMZYRO/unit/zyro_send_img.py
+++ TEAMZYRO/unit/zyro_send_img.py
@@ -7,6 +7,24 @@ from telegram.ext import CallbackContext
 
 log = "-1002946070634"
 
+rarity_map = {
+    1: "⚪️ Low",
+    2: "🟠 Medium",
+    3: "🔴 High",
+    4: "🎩 Special Edition",
+    5: "🪽 Elite Edition",
+    6: "🪐 Exclusive",
+    7: "💞 Valentine",
+    8: "🎃 Halloween",
+    9: "❄️ Winter",
+    10: "🏖 Summer",
+    11: "🎗 Royal",
+    12: "💸 Luxury Edition",
+    13: "🍃 echhi",
+    14: "🌧️ Rainy Edition",
+    15: "🎍 Festival"
+}
+
 RARITY_WEIGHTS = {
     "⚪️ Low": (40, True),
     "🟠 Medium": (20, True),
