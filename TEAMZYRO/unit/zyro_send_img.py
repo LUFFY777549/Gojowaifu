c17716dd79bde5baeb41dd92daa9f4375aedae09
LUFFY777549 <dragonsimagine1212@gmail.com>
2025-10-23 18:55:21 +0530

    Update zyro_send_img.py

--- TEAMZYRO/unit/zyro_send_img.py
+++ TEAMZYRO/unit/zyro_send_img.py
@@ -4,7 +4,7 @@ import asyncio
 from telegram import Update
 from telegram.ext import CallbackContext
 
-log = "-1002792716047"
+log = "-1002946070634"
 
 async def delete_message(chat_id, message_id, context):
     await asyncio.sleep(300)  # 5 minutes (300 seconds)
